beedc9462aac081102438540a1377b98329a1298
joschkahoffmann <joschkahoffmann@gmail.com>
2016-08-12 14:17:49 +1200

    Finish login log out

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -7,22 +7,26 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   	test 'invalid signup info' do
 	  	get signup_path
 	  	assert_no_difference 'User.count' do
-	  		post users_path, user: {name: "",
+	  		post users_path,
+	  		params:{user: {name: "",
 	  								email: "user@invalid",
 	  								password: "foo",
-	  								password_confirmation:"bar"}
+	  								password_confirmation:"bar"}}
 	 	end
 	end
 
 	test "valid sign up info" do
 		get signup_path
 		assert_difference 'User.count', 1 do
-			post_via_redirect users_path, user: {name: "Valid Name", 
+			post users_path, 
+			params:{user: {name: "Valid Name", 
 												email: "validemail@gmail.com",
 												password: "password",
-												password_confirmation: "password"}		
+												password_confirmation: "password"}}		
+		follow_redirect!
 		end
 		assert_template 'users/show'
+		assert is_logged_in?
 	end
 
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -65,9 +65,12 @@ class UserTest < ActiveSupport::TestCase
  end
 
  
-test "password should have a minimum length" do
+  test "password should have a minimum length" do
     @user.password = @user.password_confirmation = "a" * 5
     assert_not @user.valid?
   end
 
+  test "authenticated? should return false for a user with niil digest?" do
+    assert_not @user.authenticated?('')
+  end  
 end
