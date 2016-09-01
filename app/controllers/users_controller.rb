88fc3354fe48498054a43d89b95fee2d25776a38
joschkahoffmann <joschkahoffmann@gmail.com>
2016-09-01 13:47:11 +1200

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,11 +7,13 @@ class UsersController < ApplicationController
   end
   
   def index
-    @users= User.paginate(page: params[:page])
+    @users= User.where(activated: true).paginate(page: params[:page])
   end
 
   def show
   	@user=User.find(params[:id])
+    redirect_to root_url and return unless @user.activated?
+    @microposts= @user.microposts.paginate(page: params[:page])
   end
 
   def edit
@@ -54,14 +56,6 @@ class UsersController < ApplicationController
 	private
 		def user_params
 			params.require(:user).permit(:name, :email, :password, :password_confirmation)  
-    end
-
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger]= "Please log in."
-        redirect_to login_url
-      end
     end
 
     def correct_user
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-	attr_accessor :remember_token, :activation_token, :reset_token
+	has_many :microposts, dependent: :destroy
+  attr_accessor :remember_token, :activation_token, :reset_token
 	before_save { self.email = email.downcase}
 	validates :name, presence: true, length: { maximum: 50}
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -48,8 +49,8 @@ class User < ApplicationRecord
 
     def create_reset_token
       self.reset_token= User.new_token
-      update_attribute(:reset_digest, User.digest(reset_token))
-      update_attribute(:reset_sent_at, Time.zone.now)
+      update_columns(reset_digest: User.digest(reset_token),
+        reset_sent_at: Time.zone.now)
     end
     
     def send_password_reset_email
@@ -62,8 +63,12 @@ class User < ApplicationRecord
 
     def create_reset_digest
       self.reset_token= User.new_token
-      update_attribute(:reset_digest, User.digest(reset_token))
-      update_attribute(:reset_sent_at, Time.zone.now)     
+      update_columns(reset_digest: User.digest(reset_token),
+                      reset_sent_at: Time.zone.now)     
+    end
+
+    def feed
+      Micropost.where("user_id= ?", id)
     end
     
   private
